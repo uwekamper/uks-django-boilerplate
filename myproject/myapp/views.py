9e535c230b552dc068f24a7129c97baf9870c372
Uwe Kamper <me@uwekamper.de>
2013-11-03 21:58:40 +0100

    javascript is imported correctly

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,1 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 # Create your views here.
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'myapp/index.html', {})
